764905552009d74d51b1ef0784b996ed75315c8e
Ramona <ramonalazdina@gmail.com>
2022-11-09 09:03:10 +0200

    changed map key from String to Integer

--- src/main/java/letterFrequency/FrequencyOfTheLetters.java
+++ src/main/java/letterFrequency/FrequencyOfTheLetters.java
@@ -6,44 +6,40 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class FrequencyOfTheLetters {
     public static final String PATH_BEGINNING = "/textsForLetterFrequency/";
-    private static final String ENGLISH_LETTERS = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z";
+    private static final String ENGLISH_LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public Map<Character, Integer> generateFrequencyVocabulary(String nameOfTheFile) throws IOException, URISyntaxException {
 
-    public static Map<String, Integer> generateFrequencyVocabulary(String nameOfTheFile) throws IOException, URISyntaxException {
-        String[] letterArray = ENGLISH_LETTERS.split(" ");
-
-        Map<String, Integer> frequencyByLetter = new HashMap<>();
-        for (String letter : letterArray) {
-            frequencyByLetter.put(letter, 0);
-        }
+        Map<Integer, Integer> frequencyByLetter = ENGLISH_LETTERS.chars().boxed().collect(Collectors.toMap(c -> c, c -> 0));
 
         Path fullPath = Paths.get(FrequencyOfTheLetters.class.getResource(PATH_BEGINNING + nameOfTheFile).toURI());
         BufferedReader in = new BufferedReader(new FileReader(fullPath.toFile()));
 
         String line;
         while ((line = in.readLine()) != null) {
-            String[] characters = line.toUpperCase().split("");
-            for (String character : characters) {
-                if (frequencyByLetter.containsKey(character)) {
-                    frequencyByLetter.replace(character, frequencyByLetter.get(character) + 1);
+            line.toUpperCase().chars().forEach(c -> {
+                if (frequencyByLetter.containsKey(c)) {
+                    frequencyByLetter.replace(c, frequencyByLetter.get(c) + 1);
                 }
-            }
+            });
         }
 
         in.close();
 
-        return frequencyByLetter;
+        Map<Character, Integer> frequencyByCharacter = frequencyByLetter.entrySet()
+                .stream()
+                .collect(Collectors.toMap(c -> (char)c.getKey().byteValue(), Map.Entry::getValue));
+
+        return frequencyByCharacter;
 
     }
 }
 
-//    Izmantojot kādu no programmēšanas valodām, izveido angļu alfabēta burtu frekvences vārdnīcu.
-//    Analīzei izvēlies tekstu vismaz 1000 zīmju garumā.
-//
-//    Risinājumā jābūt iekļautam analizētajam tekstam TXT formātā, automatizētam testam un izpildes instrukcijai.
 
 
